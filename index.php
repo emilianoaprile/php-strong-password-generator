7b068d58fe628cd5aa4ab32a7862d31488a77beb
EmilianoAprile <emilianoaprile77@icloud.com>
2023-11-23 16:51:53 +0100

    ADDED - password generated

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
 
+$password_length = intval($_GET['user_password']);
+var_dump($password_length);
+
+// function
+function generateRandomPassword($password_length) {
+    //Inizializzo una random password
+    $password = '';
+    
+    for ($i = 0; $i < $password_length; $i++) {
+        $password .= chr(rand(32, 126));
+
+    }
+    return $password;
+  };
+  $password = generateRandomPassword($password_length);
+  var_dump($password);
+
+
 ?>
 
 
@@ -11,6 +29,18 @@
     <title>Password generator</title>
 </head>
 <body>
+    <div>
+
+        <h1>Password Generator</h1>
+        <p>Scrivi la lunghezza della password</p>
+
+        <form action="" method="GET">
+            <input type="text" name="user_password">
+            <input type="submit" value="Invia">
+        </form>
+
+    </div>
+    
     
 </body>
 </html>
